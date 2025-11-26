3f67bf222462fecfa04555e9f5042e024b51f57b
ffasdd <nhc0830@naver.com>
2025-11-27 01:26:54 +0900

    Add player animator and sprite flipping logic

--- Assets/Script/PlayerMove.cs
+++ Assets/Script/PlayerMove.cs
@@ -6,17 +6,32 @@ public class Player : MonoBehaviour
 {
     [SerializeField] float moveSpeed = 5;
     Rigidbody2D rb;
+    SpriteRenderer spriteRenderer;
+    Animator animator;
     float inputX;
 
     void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        animator = GetComponent<Animator>();
         rb.constraints = RigidbodyConstraints2D.FreezeRotation;
     }
 
     void Update()
     {
         inputX = Input.GetAxisRaw("Horizontal");
+        
+        spriteRenderer.flipX = inputX < 0;
+
+        if(inputX != 0)
+        {
+            animator.SetBool("IsWalking", true);
+        }
+        else
+        {
+            animator.SetBool("IsWalking", false);
+        }
     }
 
     void FixedUpdate()
