2a964d0fed1f0b92837c1e14cd84cbd1faab9421
ffasdd <nhc0830@naver.com>
2026-01-06 13:48:28 +0900

    Migrate tile assets to Sprites and add new sprite assets

---
+++ Assets/Script/MapLoader.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapLoader : MonoBehaviour
+{
+     public Sprite mySprite; 
+
+    // private 변수도 [SerializeField] 특성을 사용하면 인스펙터에 노출됩니다.
+    [SerializeField] private Sprite anotherSprite; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
