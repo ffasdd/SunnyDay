03040f7bc59a4f29ac0a16b224d87c10e5e07594
ffasdd <nhc0830@naver.com>
2026-02-12 15:01:47 +0900

    Add PlayerHealth and light damage over time

--- Assets/Script/Light.cs
+++ Assets/Script/Light.cs
@@ -9,6 +9,9 @@ public class Light : MonoBehaviour
     [SerializeField] private float startAngle = 0f; // 시작 각도 (도 단위)
     [SerializeField] private float endAngle = 180f; // 끝 각도 (도 단위) - 반원 기본값
     
+    [SerializeField] private float damagePerSecond = 1f;
+
+    private float damageTimer = 0f;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,27 +20,45 @@ public class Light : MonoBehaviour
 
     // Update is called once per frame
     void Update()
-{
-    float angleRange = endAngle - startAngle;
-    float angleStep = angleRange / (rayCount - 1);
-
-    for (int i = 0; i < rayCount; i++)
     {
-        float angle = (startAngle + i * angleStep) * Mathf.Deg2Rad;
-        Vector2 direction = new Vector2(Mathf.Sin(angle), Mathf.Cos(angle));
+        damageTimer += Time.deltaTime;
 
-        Debug.DrawRay(transform.position, direction * rayDistance, Color.red);
+        bool isPlayerHit = false;
 
-        RaycastHit2D hit = Physics2D.Raycast(transform.position, direction, rayDistance);
+        float angleRange = endAngle - startAngle;
+        float angleStep = angleRange / (rayCount - 1);
 
-        if (hit.collider != null)
+        for (int i = 0; i < rayCount; i++)
         {
-            if (hit.collider.CompareTag("Player"))
+            float angle = (startAngle + i * angleStep) * Mathf.Deg2Rad;
+            Vector2 direction = new Vector2(Mathf.Sin(angle), Mathf.Cos(angle));
+
+            // Debug.DrawRay(transform.position, direction * rayDistance, Color.red);
+
+            RaycastHit2D hit = Physics2D.Raycast(transform.position, direction, rayDistance);
+
+            if (hit.collider != null)
             {
-                Debug.Log("Player hit");
+                if (hit.collider.CompareTag("Player"))
+                {
+                    // Debug.Log("Player hit");
+                    isPlayerHit = true;
+                    break;
+                }
             }
-        }
+
+            
+       
     }
+
+    // 플레이어가 hit 상태이고 1초 이상이 지났다면
+            if (isPlayerHit && damageTimer >= 1f)
+            {
+                PlayerHealth playerHealth = FindFirstObjectByType<PlayerHealth>();
+                playerHealth.TakeDamage(damagePerSecond);
+                damageTimer = 0f;
+                
+            }
 }
 
 }
---
+++ Assets/Script/PlayerHealth.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public float maxhealth = 100f;
+    public float currenthealth;
+    // Start is called before the first frame update
+    void Start()
+    {
+        currenthealth = maxhealth;
+    }
+
+    public void TakeDamage(float damage){
+        currenthealth -= damage;
+        currenthealth = Mathf.Clamp(currenthealth, 0, maxhealth);
+
+        Debug.Log("현재 체력: " + currenthealth);
+
+        if(currenthealth <= 0){
+            Die();
+        }
+    }
+
+    public void Die(){
+        Debug.Log("Player is dead");
+    }
+
+}
