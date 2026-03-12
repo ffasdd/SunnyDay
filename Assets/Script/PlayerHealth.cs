8962479be2b1faf424d238389b4f2f2b8f4e606c
ffasdd <nhc0830@naver.com>
2026-03-12 20:52:22 +0900

    Add HP UI elements and dynamic HP bar

--- Assets/Script/PlayerHealth.cs
+++ Assets/Script/PlayerHealth.cs
@@ -6,23 +6,42 @@ public class PlayerHealth : MonoBehaviour
 {
     public float maxhealth = 100f;
     public float currenthealth;
-
+    public float maxHPBarWidth;
     [SerializeField] UIManagerScript uiManager;
+    [SerializeField] RectTransform hPBar;
+    
     // Start is called before the first frame update
     void Start()
     {
         currenthealth = maxhealth;
+
+        if (hPBar != null)
+        {
+            maxHPBarWidth = hPBar.sizeDelta.x;
+        }
     }
 
     public void TakeDamage(float damage){
         currenthealth -= damage;
         currenthealth = Mathf.Clamp(currenthealth, 0, maxhealth);
 
+        UpdateHPBar();
+
         // Debug.Log("현재 체력: " + currenthealth);
 
         if(currenthealth <= 0){
             Die();
         }
+    }
+
+    void UpdateHPBar(){
+        if(hPBar == null ) return;
+
+        float ratio = currenthealth / maxhealth;
+
+        Vector2 size = hPBar.sizeDelta;
+        size.x = maxHPBarWidth * ratio;
+        hPBar.sizeDelta = size;
     }
 
     public void Die(){
