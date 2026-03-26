1cbb83ce57b06251496292cfe35b98951578d637
ffasdd <nhc0830@naver.com>
2026-03-26 22:54:53 +0900

    Add SoundManager and import audio assets

---
+++ Assets/Script/SoundManager.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SoundManager : MonoBehaviour
+{
+    public static SoundManager Instance;
+    [SerializeField] private AudioSource bgmSource;
+    [SerializeField] private AudioClip inGameBGM;
+    private void Awake()
+    {
+        Instance = this;
+    }    
+    void OnEnable()
+    {
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+
+    void OnDisable()
+    {
+        SceneManager.sceneLoaded -= OnSceneLoaded;
+    }
+
+    public void PlayBGM(AudioClip clip)
+    {
+        bgmSource.clip = clip;
+        bgmSource.Play();
+    }
+    void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        if (scene.name == "InGame")
+        {
+            Debug.Log("play");
+            PlayBGM(inGameBGM);
+        }
+    }
+}
